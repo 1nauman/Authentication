de53adff63a3641e24e4b0ba16dca1e01e027727
numan-cmit <nauman.r@comakeit.com>
2020-01-21 21:37:20 +0530

    Completed Basics Claims auth Added Identity based sample project

---
+++ Basics/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Basics.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            var naumanClaims = new[] {
+                new Claim(ClaimTypes.Name, "Nauman"),
+                new Claim(ClaimTypes.Email, "n.r@live.in"),
+                new Claim("Nauman.Claim", "Good one!")
+            };
+            
+            var aadhaarClaims = new[] { new Claim("aadhaar.number", "ABCDEF"), new Claim("aadhaar.enrollment-date", "2010-01-01") };
+
+            var naumanIdentity = new ClaimsIdentity(naumanClaims, "nauman identity");
+            var aadhaarIdentity = new ClaimsIdentity(aadhaarClaims, "aadhaar identity");
+            
+            var naumanPrincipal = new ClaimsPrincipal(new[] { naumanIdentity, aadhaarIdentity });
+
+            HttpContext.SignInAsync(naumanPrincipal);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- IdentityExample/Startup.cs
+++ IdentityExample/Startup.cs
@@ -1,9 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Basics
+namespace IdentityExample
 {
     public class Startup
     {
@@ -11,11 +16,6 @@ namespace Basics
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication("Cookie.Auth").AddCookie("Cookie.Auth", cfg => {
-                cfg.Cookie.Name = "Naumans.Cookie";
-                cfg.LoginPath = "/Home/Authenticate";
-            });
-            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,7 +26,15 @@ namespace Basics
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting().UseAuthentication().UseAuthorization().UseEndpoints(e => e.MapDefaultControllerRoute());
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
         }
     }
 }
